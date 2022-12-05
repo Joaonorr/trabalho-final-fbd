ff6a05754b8cc31204f3fc1ba58ceedfc270dc8f
Joaonorr <joaonoroes.jn@gmail.com>
2022-12-05 10:25:38 -0300

    feat: create tables script

---
+++ db/bd.sql
@@ -0,0 +1,76 @@
+CREATE SCHEMA posto_saude_vacina;
+
+- TABELAS
+
+CREATE TABLE IF NOT EXISTS agente_de_saude (
+	agente_id SERIAL PRIMARY KEY,
+	nome VARCHAR(100),
+	cpf VARCHAR(11),
+);
+
+CREATE TABLE IF NOT EXISTS posto_Agente (
+	posto_agente_id INTEGER,
+	posto_de_saude_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS posto_de_saude (
+	posto_de_saude_id SERIAL PRIMARY KEY,
+	endereco_posto_id INTEGER,
+	nome_posto VARCHAR(100),
+	telefone_posto VARCHAR(11)
+);
+
+CREATE TABLE IF NOT EXISTS endereco (
+	endereco_id SERIAL PRIMARY KEY,
+	cep VARCHAR(8),
+	bairro VARCHAR(20),
+	rua VARCHAR(50),
+	numero INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS posto_lote (
+	posto_de_saude_id INTEGER,
+	lote_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS lote (
+	lote_id INTEGER,
+	qtd_vacina INTEGER,
+	data_validade DATE,
+);
+
+CREATE TABLE IF NOT EXISTS lote_vacina (
+	lote_id INTEGER,
+	vacina_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS vacina (
+	vacina_id INTEGER,
+	nome_vacina VARCHAR(100),
+	quantidade INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS fabricamte_vacina (
+	fabricante_id INTEGER,
+	vacina_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS fabricante (
+	fabricante_id INTEGER,
+	nome_fabricante VARCHAR(100),
+	telefone VARCHAR(11)
+);
+
+CREATE TABLE IF NOT EXISTS lote_cidadao (
+	lote_id INTEGER,
+	cidadao_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS cidadao (
+	cidadao_id INTEGER,
+	nome VARCHAR(100),
+	cpf VARCHAR(11),
+	data_nascimento DATE,
+	endereco_id INTEGER
+)
